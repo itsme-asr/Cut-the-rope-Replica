b61edaa18d4a7ff8d6a5611f96ba68e4f892bcab
A.S.Rawat <adityasrwt5@gmail.com>
2022-12-28 23:27:36 +0530

    Cut the rope

---
+++ Cut the rope/Assets/Script/Weight.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weight : MonoBehaviour
+{
+    public float distanceFromEnd = .6f;
+    public void connectRopeEnd(Rigidbody2D rgb)
+    {
+        HingeJoint2D joint = gameObject.AddComponent<HingeJoint2D>();
+        joint.connectedBody = rgb;
+        joint.anchor = Vector2.zero;
+        joint.connectedAnchor = new Vector2(0f, -distanceFromEnd);
+
+    }
+
+}
