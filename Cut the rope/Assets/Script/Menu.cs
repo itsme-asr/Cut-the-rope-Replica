860d6d5678ac2132158a1964ba4958ece9ff94a0
A.S.Rawat <adityasrwt5@gmail.com>
2023-01-07 19:17:53 +0530

    Cut the rope

--- Cut the rope/Assets/Script/Menu.cs
+++ Cut the rope/Assets/Script/Menu.cs
@@ -5,19 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] AudioSource playAudio;
     public void exitGame()
     {
         Debug.Log("Namaste World");
+        playAudio.Play();
         Application.Quit();
     }
 
     public void nextLevel()
     {
+        playAudio.Play();
         Invoke("Play", 1);
     }
 
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+
+    public void playStartAudio()
+    {
+        playAudio.Play();
     }
 }
